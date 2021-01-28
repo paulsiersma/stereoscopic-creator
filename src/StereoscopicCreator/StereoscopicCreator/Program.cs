25e8997e123efc1827fa478f3ce556ce87b1d0ce
Paul <paul@liquidlogic.eu>
2021-01-28 16:05:50 +0100

    Initial commit

---
+++ src/StereoscopicCreator/StereoscopicCreator/Program.cs
@@ -0,0 +1,26 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace StereoscopicCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.Directory.CreateDirectory("output");
+
+            using (Image imageL = Image.Load("C:/Repos/stereoscopic-creator/testdata/stereo_1_L.png"))
+            using (Image imageR = Image.Load("C:/Repos/stereoscopic-creator/testdata/stereo_1_R.png"))
+            using (Image image = new Image<Rgba32>(5332,6000))
+            {
+                image.Mutate(o => o
+                    .DrawImage(imageL, new Point(0, 0), 1f)
+                    .DrawImage(imageR, new Point(0, 3000), 1f)
+                );
+
+                image.Save("C:/Repos/stereoscopic-creator/testdata/ouput.png");
+            }
+        }
+    }
+}
